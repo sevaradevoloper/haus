2771b3bf64d4b05d35e99533db722d8768b5686e
sevaradeveloper <sevara123@icloud.com>
2025-10-26 22:34:13 +0300

    qolgani

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Navbar scroll blur
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 80) header.classList.add("scrolled");
+  else header.classList.remove("scrolled");
+});
+
+// Hero image slider
+const heroImage = document.getElementById("heroImage");
+const bars = document.querySelectorAll(".bar");
+const images = [
+  "./img/boka.svg",
+  "./img/hero.svg",
+  "./img/hero.svg"
+];
+let index = 0;
+setInterval(() => {
+  index = (index + 1) % images.length;
+  heroImage.style.backgroundImage = `url('${images[index]}')`;
+  // Fixed syntax
+  bars.forEach((b, i) => b.classList.toggle("active", i === index));
+}, 5000);
+
+// Burger menu
+const burger = document.getElementById("burger");
+const navLinks = document.querySelector(".header__links"); // Barcha linklarni o'z ichiga olgan asosiy ul
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  navLinks.classList.toggle("menu-open"); // Toggle a class to show/hide menu
+});
+
+// **YANGI: Linkni bosganda menyuni yopish**
+navLinks.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    // Agar menyu ochiq bo'lsa, uni yopamiz
+    if (navLinks.classList.contains("menu-open")) {
+      burger.classList.remove("active");
+      navLinks.classList.remove("menu-open");
+    }
+  });
+});
